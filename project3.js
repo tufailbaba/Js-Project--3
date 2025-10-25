c20ce43d3ec951fdc20dd5745b6a9cf162b0d9bc
Tufail <tufailbabacoc@gmail.com>
2025-10-25 11:36:25 +0530

    Contact management system

---
+++ project3.js
@@ -0,0 +1,12 @@
+const promt = require("prompt-sync")();
+
+function printInfo() {
+  console.log("Contact Management System");
+  console.log("---------------");
+  console.log("1. Add a contact");
+  console.log("2. Delete a contact");
+  console.log("3. View Contacts");
+  console.log("4. Search Contacts");
+  console.log("5. Exit");
+}
+printInfo()
